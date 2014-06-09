61dda0b3150f261b8a6e0ddf9b3ea102b1f8a0c7
mcpe-gamer <Alvarezcarlos232@gmail.com>
2014-06-09 08:11:41 -0500

    Started Dev for SignPlayerLimit.php

---
+++ SignPlayerLimit.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+__Pocketmine Plugin__
+Name=Sign Player Limit
+Description=Sets a limit on how many players can be on a world through a sign
+Version=1.0 Dev
+Author=Plugin-Devs-Forever Team
+Class=Limit
+apiversion=12
+*/
+
+/*
+Remove Dev when Plugin Development is complete.
+xxxxxxxxxxxxxxxx
+CHANGELOG
+xxxxxxxxxxxxxxxx
+1.0 Dev Init start of Development
+xxxxxxxxxxxxxxxx
+*/
+
+class Limit implements plugin{
+Private $api
+
+public function __construct(ServerAPI $api, $server = false){
+$this->api = $api
+
+}
+
+public function init(){
+
+
+
+}
+public function __destruct(){
+
+}
+
+}
